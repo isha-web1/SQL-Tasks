d7ad309dfa18a0c52d67b6f4050d3d628486361c
isha-web1 <isha.web1@gmail.com>
2025-08-16 21:08:58 +0600

    insert data & create table

--- solution.sql
+++ solution.sql
@@ -5,3 +5,24 @@ CREATE TABLE departments (
     department_name VARCHAR(100) NOT NULL,
     manager_id INT
 );
+
+
+-- create employees table
+CREATE TABLE employees (
+    employee_id SERIAL PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    hire_date DATE NOT NULL,
+    salary NUMERIC(10, 2) NOT NULL,
+    department_id INT REFERENCES departments (department_id),
+    is_active BOOLEAN
+);
+
+-- insert data into departments table
+INSERT INTO
+    departments (department_name, manager_id)
+VALUES ('Human Resources', 1),
+    ('Finance', 2),
+    ('Engineering', 4);
+
+SELECT * FROM departments;
